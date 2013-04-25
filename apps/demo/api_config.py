315041210f59b31bf2cff32f5f69e9afb15ff71c
fatelei <fatelei@gmail.com>
2013-04-25 16:17:59 +0800

    x

--- apps/demo/api_config.py
+++ apps/demo/api_config.py
@@ -2,3 +2,5 @@
 #-*-coding: utf8-*-
 
 API_DOMAIN = "localhost:9000"
+CLIENT_SECRET = '0084697bc0104e74ba0a9ad4de632552'
+CLIENT_KEY = '1bc16f8d72fa4547aafa233bf0ad7331'
--- apps/demo/views/user.py
+++ apps/demo/views/user.py
@@ -7,4 +7,22 @@ from flask import request, render_template
 @app.route("/demo/login", methods = ['GET', 'POST'])
 def oauth_login():
 	if request.method == 'POST':
+		pass
+	else:
+		pass
+
+@app.route("/demo/register", method = ['GET', 'POST'])
+def oauth_register():
+	if request.method == 'POST':
+		pass
+	else:
+		pass
+
+@app.route("/demo/logout", method = ['GET'])
+def oauth_logout():
+	pass
+
+@app.route("/demo/pwdchange", method = ['GET', 'POST'])
+def change_password():
+	pass
 		
