3e1eca915aa80443ec6f42f0bce25d4678d2634e
fatelei <fatelei@gmail.com>
2013-04-25 17:56:50 +0800

    x

--- apps/demo/utils/auth.py
+++ apps/demo/utils/auth.py
@@ -1,11 +1,14 @@
 #!/usr/bin/env python
 #-*-coding: utf8-*-
 
+import urllib
 import httplib2
 
-
 def api_auth(email, password):
-	pass
+	data = {"email": email, "password": password}
+	headers = {"Content-Type": "application/x-www-form-urlencoded"}
+	body = urllib.urlencode(data)
+	http = 
 
 def api_register(data):
 	pass
